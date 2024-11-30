6242f8966f4e7f58e227d28c44496349180e9fe8
chienbd7797 <buidinhchien.cdt@gmail.com>
2024-11-30 13:35:46 +0700

    tạo API login/register/logout

--- tasks/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Trang chủ
-    path('login/', views.auth_page, name='login'),  # Trang đăng nhập/đăng ký
-]
-
-
